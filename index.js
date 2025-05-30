120ec019be61158120000e6ef84f4daea51bc555
ngolrva <ngovannamt36@gmail.com>
2025-05-30 14:00:36 +0700

    second-version

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const app = express();
 
 app.use(express.json());
+app.use(express.static('dist'))
 morgan.token("content", function getContent (req) { return JSON.stringify(req.body)})
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time[digits] ms :content'));
